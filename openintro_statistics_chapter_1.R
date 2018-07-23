4c5a5a713fd1321debcc793eba6fb22484859a4d
Marceldb <Marcello.delbono@gmail.com>
2018-07-23 14:16:13 +0200

    Add files via upload

--- openintro_statistics_chapter_1.R
+++ openintro_statistics_chapter_1.R
@@ -7,6 +7,8 @@ library(openintro)
 library(tidyverse)
 library(magrittr)
 
+
+
 # viewing datasets from openintro
 data(package="openintro")
 
@@ -118,3 +120,139 @@ table(stratified_sample$Species) / nrow(stratified_sample)
 #     setosa versicolor  virginica 
 #       0.5        0.3        0.2 
 
+tbl<-email50 %>% 
+    mutate(bin = ntile(num_char, 8)) %>% 
+  group_by(bin) %>%
+  count() 
+?cut_interval
+email50 %>% 
+  mutate(bin = ntile(num_char, 8)) %>% View()
+
+# --- 1.6.3 HIstograms and Shape ----
+
+# tbl 1.21
+# selecting by num_chars and assigning to a segment
+# probably can be done easier with binning functions
+
+tbl_1_21<-email50
+mybreak<- c(0,5,10,15,20,25,30,35,40,45,50,55,60,65)
+# use labels parameter if you want to change the labels such as "5-10" etc
+tbl_1_21$segmento<-NA
+tbl_1_21$segmento<-cut(email50$num_char, breaks = mybreak)
+
+# or
+tbl_1_21$segmento<-NA
+tbl_1_21$segmento<-cut(email50$num_char, seq(from =0, to = 65, by = 5))
+
+
+
+# table 1.21 tidy version
+tbl_1_21 %<>% 
+  group_by(segmento) %>% 
+  summarise(n = n())
+
+
+# table 1.21 with a column for 
+# any bin, same as the book
+tbl_1_21_s<-tbl_1_21 %>% spread(segmento,n)
+tbl_1_21_s
+
+# Histogram fig. 1.22
+email50 %>% 
+  ggplot()+
+  geom_histogram(aes(num_char))
+
+bfill <- "skyblue4"
+bline <- "grey3"
+ggplot(email50, aes(x=num_char)) + 
+  geom_histogram( binwidth = 5,colour = bline, fill = bfill)+
+  labs(caption = "Figure 1.22: A histogram of num char. This distribution is very strongly skewed to the right.")+
+  labs(x = "Number of Characters (in thousands)")+
+  labs(y = "Frequency") +
+  theme_light()+
+  theme(plot.caption = element_text(hjust = 0.5))
+
+#  see also https://rdrr.io/cran/openintro/man/
+
+# Fig 1.29 (a)
+MLB %>% 
+  ggplot()+
+  geom_histogram(aes(salary))
+
+bfill <- "skyblue4"
+bline <- "grey3"
+ggplot(MLB, aes(x=salary/1000)) + 
+  geom_histogram( binwidth = 5,colour = bline, fill = bfill)+
+  labs(caption = "Figure 1.29: (a) Histogram of MLB player salaries for 2010, in millions of dollars")+
+  labs(x = "Salary (millions of dollars)\n(a)")+
+  labs(y = "") +
+  theme_light()+
+  theme(plot.caption = element_text(hjust = 0.5))
+
+
+# Fig 1.29 (b)
+MLB %>% 
+  ggplot()+
+  geom_histogram(aes(salary))
+
+bfill <- "skyblue4"
+bline <- "grey3"
+ggplot(MLB, aes(x=salary/1000)) + 
+  geom_histogram( binwidth = 5,colour = bline, fill = bfill)+
+  labs(caption = "Figure 1.29: (b) Histogram of the log-transformed MLB player salaries for 2010.")+
+  labs(x = "loge(Salary), where Salary is in millions USD\n(b)")+
+  labs(y = "") +
+  theme_light()+
+  theme(plot.caption = element_text(hjust = 0.5))
+
+#----1.6.4 Variance and Standard Deviation----
+# Manually calculating deviation as xi-xavg with a for loop
+calc.deviation = function(x) {
+  avg<-mean(x)
+    for (i in 1:50) {
+    print(x[i]-avg)
+    }
+}
+calc.deviation(email50$num_char)
+
+# Manually calculating deviation as xi-xavg with r vectorization
+calc.deviation = function(x) {
+  avg<-mean(x)
+  mydeviation<-0
+  mydeviation<-(x-avg)
+  print(mydeviation)
+}
+
+calc.deviation(email50$num_char)
+
+# Sample Variance s2
+# manually calculating sample variance s2 with a for loop
+calc.smpl.variance = function(x) {
+  avg<-mean(x)
+  endloop<-length(email50$num_char)
+  sv<-0
+  for (i in 1:endloop) {
+    sv<-sv+(x[i]-avg)^2
+    print(sv)
+  }
+  sv<-sv/(endloop-1)
+  mytext<-paste("s2 sample variance is", sv)
+  print(mytext)
+}
+calc.smpl.variance(email50$num_char)
+# Manually calculating sample variance s2 with r vectorization
+calc.smpl.variance = function(x) {
+  avg<-mean(x)
+  sv<-0
+  mydenominator<-length(x)
+  sv<-sum((x-avg)^2)
+  sv<-sv/(mydenominator-1)
+  mytext<-paste("s2 sample variance is", sv)
+  print(mytext)
+}
+calc.smpl.variance(email50$num_char)
+
+# Check_ calculating sample variance with r function  (n-1 in the denominator)
+var(email50$num_char) 
+# 172.2725 same as the manual function 
+# the book says 172,44 probably an error 
